bfd9083b40252dd671379b6e38c4ebf5fff200c3
shruthasivakumar <shrutha.sivakumar@gmail.com>
2017-06-17 15:37:43 +0200

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+def hi(name):
+    if name == 'Shrutha':
+        print('Hi Shrutha!')
+        print('Whats up?')
+    elif name == 'a':
+        print ('Hi A!')
+    else:
+        print ('Go away random person!')
+
+hi('b')
